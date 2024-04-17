b54942c961b8834e123b221699e9760306ab9abf
Yoann Gendron-Perrault <yoann.gendron-perrault@icosolutions.com>
2024-04-17 11:56:36 -0400

    Add default style

--- src/app.component.ts
+++ src/app.component.ts
@@ -5,6 +5,20 @@ import {Component} from '@angular/core';
     template: `
         Welcome to angular training
     `,
+    styles: [
+        `
+            :host {
+                background: linear-gradient(120deg, #b92c8e, #662177);
+                width: 100%;
+                height: 100%;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                font-size: 30px;
+                color: #FEBD17;
+            }
+        `
+    ],
     standalone: true
 })
 export class AppComponent {}
