0563ba96eed004aeda3a03d1d3e9b8536dace4b3
Yoann Gendron-Perrault <yoann.gendron-perrault@icosolutions.com>
2024-04-17 09:04:50 -0400

    Remove stuff for now

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,46 +1,10 @@
-import {Component, Directive, ElementRef, Inject, Injector, Input} from '@angular/core';
-import {UpgradeComponent} from '@angular/upgrade/static';
-
-@Directive({
-    // eslint-disable-next-line @angular-eslint/directive-selector
-    selector: 'my-ajs-control',
-    standalone: true
-})
-export class MyAjsControlComponentDirective extends UpgradeComponent {
-    @Input() public myValue: any;
-    @Input() public myStringValue?: string;
-    @Input() public myFunction?: () => void;
-
-    constructor(elementRef: ElementRef, injector: Injector) {
-        super('myAjsControl', elementRef, injector);
-    }
-}
+import {Component, Inject} from '@angular/core';
 
 @Component({
     selector: 'app',
     template: `
         Welcome to angular training
-        <my-ajs-control
-            [myValue]="4"
-            [myStringValue]="stringValue"
-            [myFunction]="onClickButton"
-        />
     `,
-    imports: [MyAjsControlComponentDirective],
     standalone: true
 })
-export class AppComponent {
-    protected stringValue = 'base';
-
-    constructor(
-        @Inject('$scope') private $scope: angular.IScope
-    ) {
-        this.onClickButton = this.onClickButton.bind(this);
-    }
-
-    protected onClickButton() {
-        Promise.resolve().then(() => {
-            this.stringValue = `${this.stringValue}1`;
-        });
-    }
-}
+export class AppComponent {}
